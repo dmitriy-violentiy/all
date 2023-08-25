9220ca011d3e1f4284475493d98f9b2b76f0faab
violentiy1997 <violentiy1997@mail.ru>
2023-08-25 14:09:45 +0300

    add modile window without animate

--- all/src/App.js
+++ all/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Count from "./components/Count/Count";
+import Modal from "./components/Modal/Modal";
 
 function App() {
 	return (
 		<div className="App">
 			<Count />
+			<Modal />
 		</div>
 	);
 }
---
+++ all/src/components/Modal/Modal.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import classes from "./Modal.module.css";
+
+const Modal = () => {
+	const [open, setOpen] = useState(false);
+
+	return (
+		<div>
+			<button
+				onClick={() => {
+					setOpen(true);
+				}}
+				className={classes.openModalBtn}
+			>
+				Открыть неаминированное модальное окно
+			</button>
+			{open && (
+				<div className={classes.overlay}>
+					<div className={classes.modal}>
+						<span
+							onClick={() => {
+								setOpen(false);
+							}}
+							className={classes.closeModal}
+						>
+							X
+						</span>
+						<p>ds s</p>
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default Modal;
